d531e380ebfa4d47e75853a4d329d47656968cde
vipin.kumar03 <vipin.kumar03@motherson.com>
2025-02-04 18:26:52 +0530

    RestTemplate implemented.

--- UserService/src/main/java/com/vip/user/service/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/vip/user/service/impl/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package com.vip.user.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.vip.user.entity.Ratings;
 import com.vip.user.entity.User;
 import com.vip.user.repository.UserRepository;
 import com.vip.user.service.UserService;
@@ -15,6 +18,9 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	RestTemplate restTemplate;
 	
 	@Override
 	public User saveUser(User user) {
@@ -26,12 +32,17 @@ public class UserServiceImpl implements UserService {
 		return userRepository.findAll();
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public User getUser(String userId) {
 		Optional<User> findById = userRepository.findById(userId);
 		
-		if(findById.isPresent())
-			return findById.get();
+		if(findById.isPresent()) {
+			 User user = findById.get();
+			 List<Ratings> ratings = restTemplate.getForObject("http://localhost:7073/ratings/getRatingByUser?userId="+userId, ArrayList.class);
+			 user.setRatings(ratings);
+			 return user;
+		}
 		else
 			return null;
 	}
